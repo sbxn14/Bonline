6c1f18c19dc58f0c1512b35ed068c9dbb190f91b
Unknown <s.westerburger@student.fontys.nl>
2017-10-24 12:12:22 +0200

    Update vs
    Database klasse

---
+++ Bonline/Bonline/Database/Database.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Bonline.Database
+{
+ public class Database
+ {
+  private string connectionstring = "";
+  public List<T> RunQuery<T>(T value) where T : IQuery, new()
+  {
+   List<T> results = new List<T>();
+   using (SqlConnection con = new SqlConnection(connectionstring))
+   {
+    using (SqlCommand cmd = con.CreateCommand())
+    {
+	SqlDataReader reader = null;
+	cmd.Connection.Open();
+	cmd.Prepare();
+	reader = cmd.ExecuteReader();
+	while (reader.Read())
+	{
+
+	}
+	cmd.Connection.Close();
+    }
+   }
+   return results;
+  }
+ }
+}
+
---
+++ Bonline/Bonline/Database/IQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonline.Database
+{
+ public interface IQuery
+ {
+	 void Parse(SqlDataReader reader);
+	 string Query { get; set; }
+ }
+}
