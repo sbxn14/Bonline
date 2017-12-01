41bed83fc73265bca8004b95cedb0bb0178e6828
CyberWolf <jramackers@ictmbo.nl>
2017-12-01 09:14:21 +0100

    Auto stash before merge of "master" and "origin/master"

--- Bonline/Bonline/Database/DB.cs
+++ Bonline/Bonline/Database/DB.cs
@@ -17,7 +17,7 @@ namespace Bonline.Database
    ConnectionString = "Server=tcp:bonline.database.windows.net,1433;Database=Bon-Line;Uid=Kassabon@bonline;Pwd=Fontys2017;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;";
 =======
    //connectionstring localhost   
-   ConnectionString = "Data Source=LAPTOP-GICS0PBT;Initial Catalog=BonlineDatabase;Integrated Security=True";
+   //ConnectionString = "Data Source=LAPTOP-GICS0PBT;Initial Catalog=BonlineDatabase;Integrated Security=True";
 
 >>>>>>> Accounts view
 =======
@@ -26,7 +26,7 @@ namespace Bonline.Database
 
 >>>>>>> master
    //ConnectionString voor Azure database
-   //ConnectionString = "Server=tcp:bonline.database.windows.net,1433;Database=Bon-Line;Uid=Kassabon@bonline;Pwd=Fontys2017;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;";
+   ConnectionString = "Server=tcp:bonline.database.windows.net,1433;Database=Bon-Line;Uid=Kassabon@bonline;Pwd=Fontys2017;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;";
   }
 
   public static List<T> RunQuery<T>(T value) where T : IQuery, new()
